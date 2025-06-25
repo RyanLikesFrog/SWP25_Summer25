e362ea3170f732d69e3a0671ae1f4b1173ebb3e8
RyanLikesFrog <mtdeptrai430@gmail.com>
2025-06-26 02:06:12 +0700

    create lab result, update lab result, update medical record

--- DataLayer/Enum/ARVProtocolType.cs
+++ DataLayer/Enum/ARVProtocolType.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataLayer.Enum
+﻿namespace DataLayer.Enum
 {
     public enum ARVProtocolType
     {
-        A,
-        B,
-        C,
-        D,
-        E,
-        F,
-        G,
-        H,
-        I,
+        SimpleHIV = 0,        // HIV đơn thuần (không có bệnh phối hợp)
+        HIV_TB = 1,           // HIV đồng nhiễm Lao (Tuberculosis)
+        HIV_HBV = 2,          // HIV đồng nhiễm Viêm gan B
+        HIV_HCV = 3,          // HIV đồng nhiễm Viêm gan C
+        HIV_Sepsis = 4,       // HIV có nhiễm trùng máu
+        HIV_Kidney = 5,       // HIV + Bệnh thận
+        HIV_Liver = 6,        // HIV + Suy gan
+        HIV_Cancer = 7,       // HIV + Ung thư (Kaposi, lymphoma...)
+        HIV_Pregnancy = 8,    // HIV + Phụ nữ mang thai
+        HIV_Pediatric = 9,    // HIV ở trẻ em
     }
 }
