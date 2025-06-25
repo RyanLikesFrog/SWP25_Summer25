0c7fa66cb801b730e5882dbfab421ec248981bbe
RyanLikesFrog <mtdeptrai430@gmail.com>
2025-06-26 02:06:44 +0700

    Revert "create lab result, update lab result, update medical record"
    This reverts commit e362ea3170f732d69e3a0671ae1f4b1173ebb3e8.

--- DataLayer/Enum/ARVProtocolType.cs
+++ DataLayer/Enum/ARVProtocolType.cs
@@ -1,16 +1,21 @@
-﻿namespace DataLayer.Enum
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Enum
 {
     public enum ARVProtocolType
     {
-        SimpleHIV = 0,        // HIV đơn thuần (không có bệnh phối hợp)
-        HIV_TB = 1,           // HIV đồng nhiễm Lao (Tuberculosis)
-        HIV_HBV = 2,          // HIV đồng nhiễm Viêm gan B
-        HIV_HCV = 3,          // HIV đồng nhiễm Viêm gan C
-        HIV_Sepsis = 4,       // HIV có nhiễm trùng máu
-        HIV_Kidney = 5,       // HIV + Bệnh thận
-        HIV_Liver = 6,        // HIV + Suy gan
-        HIV_Cancer = 7,       // HIV + Ung thư (Kaposi, lymphoma...)
-        HIV_Pregnancy = 8,    // HIV + Phụ nữ mang thai
-        HIV_Pediatric = 9,    // HIV ở trẻ em
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
     }
 }
